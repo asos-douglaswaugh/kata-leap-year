730f0325b4276b57832ef6632c09f904450680af
Douglas Waugh <douglas.waugh@asos.com>
2022-12-05 17:40:29 +0000

    Removes is/is not leap year tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -5,32 +5,6 @@ public class Tests
     [TestCase(1)]
     [TestCase(2)]
     [TestCase(3)]
-    [TestCase(100)]
-    [TestCase(1997)]
-    [TestCase(1800)]
-    public void Is_not_a_leap_year(int year)
-    {
-        var isLeapYear = IsLeapYear(year);
-
-        Assert.That(isLeapYear, Is.False);
-    }
-
-    [TestCase(4)]
-    [TestCase(8)]
-    [TestCase(12)]
-    [TestCase(400)]
-    [TestCase(1996)]
-    [TestCase(1600)]
-    public void Is_a_leap_year(int year)
-    {
-        var isLeapYear = IsLeapYear(year);
-
-        Assert.That(isLeapYear, Is.True);
-    }
-
-    [TestCase(1)]
-    [TestCase(2)]
-    [TestCase(3)]
     [TestCase(1997)]
     public void Non_leap_years_not_divisible_by_4(int year)
     {
