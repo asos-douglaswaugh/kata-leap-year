8dfa73e08815e6899074ebf4560688f2515b9439
Douglas Waugh <douglas.waugh@asos.com>
2022-12-05 17:39:40 +0000

    Adds more understandable tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -28,6 +28,46 @@ public class Tests
         Assert.That(isLeapYear, Is.True);
     }
 
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(3)]
+    [TestCase(1997)]
+    public void Non_leap_years_not_divisible_by_4(int year)
+    {
+        var isLeapYear = IsLeapYear(year);
+
+        Assert.That(isLeapYear, Is.False);
+    }
+
+    [TestCase(4)]
+    [TestCase(8)]
+    [TestCase(12)]
+    [TestCase(1996)]
+    public void Leap_years_divisible_by_4(int year)
+    {
+        var isLeapYear = IsLeapYear(year);
+
+        Assert.That(isLeapYear, Is.True);
+    }
+
+    [TestCase(100)]
+    [TestCase(1800)]
+    public void Non_leap_years_divisible_by_100(int year)
+    {
+        var isLeapYear = IsLeapYear(year);
+
+        Assert.That(isLeapYear, Is.False);
+    }
+
+    [TestCase(400)]
+    [TestCase(1600)]
+    public void Leap_years_divisible_by_400(int year)
+    {
+        var isLeapYear = IsLeapYear(year);
+
+        Assert.That(isLeapYear, Is.True);
+    }
+
     private static bool IsLeapYear(int year)
     {
         if (year % 400 == 0)
